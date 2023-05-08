498d906be17c1452cfd64d9bc45b97544dbbfecc
Piyush <piyushmaurya203@gmail.com>
2023-05-09 01:39:25 +0530

    Status of form

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ def home():
     p_col=-1
     if request.method == "POST":
         LDAP_ID = request.form['ldap_id']
+        findin= request.form['find']
+        
         print(f"here is ldap id {LDAP_ID}")
         gc = gspread.service_account('keys.json')
         spreadsheet = gc.open_by_key(SHEET_ID)
@@ -40,7 +42,11 @@ def home():
         print(f"This is value of p_col{p_col}")           
         
         if p_col!=-1:
-            df = pd.DataFrame({'LDAP ID': [LDAP_ID],'Lab Name': [df.columns.tolist()[p_col]], 'Custodian': [df.iloc[0, p_col]], 'PI': [df.iloc[1, p_col]]})
+            if str(findin)=='PI of Student':
+                df = pd.DataFrame({'LDAP ID': [LDAP_ID], 'PI of Student': [df.iloc[1, p_col]]})
+            if str(findin)=='Lab Name':
+                df = pd.DataFrame({'LDAP ID': [LDAP_ID],'Lab Name': [df.columns.tolist()[p_col]]})
+            
            
             data1=df.to_html(index=False,sparsify=False)
             print(data1)
@@ -67,6 +73,7 @@ def faculty():
     df = pd.DataFrame([])
     if request.method == "POST":
         LDAP_ID = request.form['ldap_id']
+        findin = request.form['find']
         
         gc = gspread.service_account('keys.json')
         spreadsheet = gc.open_by_key(SHEET_ID)
@@ -85,7 +92,13 @@ def faculty():
         print(f"This is value of p_row{p_row}")
         df_json = df.to_json(orient='records')
         if p_row!=-1:
-            df = pd.DataFrame({'Faculty Name': [df.iloc[p_row, 0]], 'No of Labs': [df.iloc[p_row, 1]], 'Lab Name': [df.iloc[p_row, 2]],'No of Student': [df.iloc[p_row, 3]]})
+            if str(findin)=='No of Student':
+                df = pd.DataFrame({'Faculty Name': [df.iloc[p_row, 0]],'No of Student': [df.iloc[p_row, 3]]})
+            if str(findin)=='No of Labs':
+                df = pd.DataFrame({'Faculty Name': [df.iloc[p_row, 0]], 'No of Labs': [df.iloc[p_row, 1]]})
+            if str(findin)=='Lab Name':
+                df = pd.DataFrame({'Faculty Name': [df.iloc[p_row, 0]], 'Lab Name': [df.iloc[p_row, 2]]})
+            
             data1=df.to_html(index=False,sparsify=False)
             print(data1)
             table_with_styles = data1.replace('<table', '<table style="border: 2px solid black;padding: 15px;"') \
@@ -159,6 +172,60 @@ def custodian():
         df_json = df.to_json(orient='records')
 
     return render_template('Custodian.html', data=df.to_html())
+@app.route("/status", methods = ['GET', 'POST'])
+def status():
+    SHEET_ID = '1pbTLKnkCcDuMIoejs_mWW8SnZVW5_Eqgk2Qs1oZgQqk'
+    # SHEET_ID = ''
+    SHEET_NAME = 'Student Data'
+    df_json=''
+    print('HELLO')
+    df = pd.DataFrame([])
+    if request.method == "POST":
+        
+        
+        gc = gspread.service_account('keys.json')
+        spreadsheet = gc.open_by_key(SHEET_ID)
+        worksheet = spreadsheet.worksheet(SHEET_NAME)
+        rows = worksheet.get_all_records()
+        # print(rows[:5])
+
+        # print('==============================')
+        df = pd.DataFrame(rows)
+        start_row = 17
+        end_row = 500
+
+# Filter the DataFrame to include only the rows in the specified range with sum of second column onwards >= 1
+        filtered_df = df.iloc[start_row:end_row+1][(df.iloc[start_row:end_row+1].iloc[:,1:].sum(axis=1)) >= 1]
+
+# Output the values of the first column for each row in the filtered DataFrame
+        list=[]
+        for index, row in filtered_df.iterrows():
+          list.append(row[0])
+        df = pd.DataFrame({'Student Status': list })
+        data1=df.to_html(index=False,sparsify=False)
+        print(data1)
+        table_with_styles = data1.replace('<table', '<table style="border: 2px solid black;padding: 15px;"') \
+    .replace('<th', '<th style="text-align: center;border-collapse: collapse;"') \
+    .replace('<td', '<td style="border: 2px solid black;padding: 15px;"')
+        return render_template('status.html', data2=table_with_styles,flag=True)
+        df_json = df.to_json(orient='records')
+        if p_col==-1:
+            df = pd.DataFrame({'Student Status': ['Student Record Not Found'] })
+            return render_template('home.html', data=df.to_html(),flag=False)
+         
+        
+
+# select first column if sum of values from second column to last column is greater than 0, starting from start_row
+#         selected_col = []
+#         for index, row in df.iterrows():
+#           if index >= start_row and row.iloc[1:].sum() > 0:
+#             selected_col.append(row.iloc[0])
+
+# # add selected column as a new column to the dataframe
+#         df['selected_col'] = selected_col
+#         print(selected_col)
+            # return render_template('home.html', data2=df.to_html(),flag=True)
+    return render_template('Status.html', data2=df.to_html(),flag=1)
 @app.route("/room", methods = ['GET', 'POST'])
 def room():
     SHEET_ID = '1pbTLKnkCcDuMIoejs_mWW8SnZVW5_Eqgk2Qs1oZgQqk'
